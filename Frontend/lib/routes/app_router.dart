13161976b6926607f75d62d432dd296550c9c736
tsomhs <georgetsomhs@gmail.com>
2024-09-02 03:02:11 +0300

    changed app router

--- Frontend/lib/routes/app_router.dart
+++ Frontend/lib/routes/app_router.dart
@@ -5,11 +5,10 @@ import 'package:mypr/routes/app_router.gr.dart';
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: LoginRoute.page, initial: false),
-        AutoRoute(page: SignUpRoute.page, initial: false),
+        AutoRoute(page: LoginRoute.page, initial: true),
+        AutoRoute(page: SignUpRoute.page),
         AutoRoute(
           page: BottomNavBarRoute.page,
-          initial: true,
           children: [
             AutoRoute(
               page: HomeNavigation.page,
