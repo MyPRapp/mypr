dace1109b3c138fc80d5f824e598e6c7a7df82a8
tsomhs <georgetsomhs@gmail.com>
2024-10-09 17:30:17 +0300

    bookings load from my bookings page only for performance boost

--- Frontend/lib/MainPages/HomePage/home_page.dart
+++ Frontend/lib/MainPages/HomePage/home_page.dart
@@ -7,7 +7,6 @@ import 'package:mypr/global_components.dart';
 import 'package:provider/provider.dart';
 
 import '../../Navigation/bottom_nav_bar.dart';
-import '../../Providers/booking_provider.dart';
 import '../../Providers/club_provider.dart';
 import '../../services/auth_service.dart';
 
@@ -30,18 +29,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _initApp() async {
-    // Run both futures concurrently
-    await Future.wait([
-      _initUser(),
-      _fetchClubs(),
-    ]);
-
-    // Now both _initUser and _fetchClubs are completed
-    if (mounted && context.read<GlobalStateProvider>().isAuthenticated) {
-      await context.read<BookingProvider>().fetchBookings(
-          context.read<UserProvider>().userDetails,
-          context.read<ClubProvider>());
-    }
+    _initUser();
+    _fetchClubs();
   }
 
   Future<void> _fetchClubs() async {
@@ -106,114 +95,111 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.sizeOf(context).height;
     final double screenWidth = MediaQuery.sizeOf(context).width;
-    double statusBarHeight = MediaQuery.viewPaddingOf(context).top;
+    // double statusBarHeight = MediaQuery.viewPaddingOf(context).top;
     return PopScope(
       canPop: false,
       child: Scaffold(
           backgroundColor: const Color.fromARGB(197, 41, 41, 41),
-          body: Padding(
-            padding: EdgeInsets.only(top: statusBarHeight),
-            child: SizedBox(
-              height: screenHeight,
-              width: screenWidth,
-              child: RefreshIndicator.adaptive(
-                color: const Color(0xFF9C0C04),
-                onRefresh: _initApp,
-                child: ListView(
-                  children: [
-                    SizedBox(
-                      height: screenHeight / 13,
-                      width: screenWidth,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: screenWidth * 0.02),
-                            child: Image.asset(
-                              'assets/otherPhotos/Logo_v2.2-removebg(cropped).png', // Replace with your logo asset path
-                              height: screenHeight / 13,
-                              width: screenWidth / 4,
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(right: screenWidth * 0.02),
-                            alignment: Alignment.center,
+          body: SizedBox(
+            height: screenHeight,
+            width: screenWidth,
+            child: RefreshIndicator.adaptive(
+              color: const Color(0xFF9C0C04),
+              onRefresh: _initApp,
+              child: ListView(
+                children: [
+                  SizedBox(
+                    height: screenHeight / 13,
+                    width: screenWidth,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: screenWidth * 0.02),
+                          child: Image.asset(
+                            'assets/otherPhotos/Logo_v2.2-removebg(cropped).png', // Replace with your logo asset path
                             height: screenHeight / 13,
-                            width: screenWidth / 2.2,
-                            child: GestureDetector(
-                              onTap: () {
-                                navigateToSearchTab(context);
-                              },
-                              child: const TextField(
-                                enabled: false,
-                                style: TextStyle(color: Colors.white),
-                                decoration: InputDecoration(
-                                  hintText: 'Αναζήτηση',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  filled: true,
-                                  fillColor: Color.fromARGB(255, 0, 0, 0),
-                                  border: OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(8)),
-                                    borderSide: BorderSide.none,
-                                  ),
-                                  prefixIcon:
-                                      Icon(Icons.search, color: Colors.grey),
+                            width: screenWidth / 4,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: screenWidth * 0.02),
+                          alignment: Alignment.center,
+                          height: screenHeight / 13,
+                          width: screenWidth / 2.2,
+                          child: GestureDetector(
+                            onTap: () {
+                              navigateToSearchTab(context);
+                            },
+                            child: const TextField(
+                              enabled: false,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintText: 'Αναζήτηση',
+                                hintStyle: TextStyle(color: Colors.grey),
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 0, 0, 0),
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8)),
+                                  borderSide: BorderSide.none,
                                 ),
+                                prefixIcon:
+                                    Icon(Icons.search, color: Colors.grey),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: screenWidth * 0.03,
-                          top: screenHeight * 0.035,
-                          bottom: screenHeight * 0.02),
-                      child: Text(
-                        'Όλα τα αποτελέσματα',
-                        style: TextStyle(
-                          fontSize: screenWidth * 0.02 + screenHeight * 0.0125,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
                         ),
-                      ),
+                      ],
                     ),
-                    Consumer<ClubProvider>(
-                      builder: (context, clubProvider, _) {
-                        return ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: clubProvider.allClubs.length,
-                          itemBuilder: (context, index) {
-                            ClubInfoStruct club = clubProvider.allClubs[index];
-                            if (club.clubPhoto.isNotEmpty ||
-                                club.localPhotoPath.isNotEmpty) {
-                              return BigClubCard(
-                                key: ValueKey(club.clubID),
-                                club: club,
-                                screenHeight: screenHeight,
-                                screenWidth: screenWidth,
-                              );
-                            }
-                            return null;
-                          },
-                        );
-                      },
-                    ),
-                    const Text(
-                      textAlign: TextAlign.center,
-                      'Περισσότερα club έρχονται σύντομα...',
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: screenWidth * 0.03,
+                        top: screenHeight * 0.035,
+                        bottom: screenHeight * 0.02),
+                    child: Text(
+                      'Όλα τα αποτελέσματα',
                       style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
+                        fontSize: screenWidth * 0.02 + screenHeight * 0.0125,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
-                    SizedBox(height: screenHeight / 40 * 6)
-                  ],
-                ),
+                  ),
+                  Consumer<ClubProvider>(
+                    builder: (context, clubProvider, _) {
+                      return ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: clubProvider.allClubs.length,
+                        itemBuilder: (context, index) {
+                          ClubInfoStruct club = clubProvider.allClubs[index];
+                          if (club.clubPhoto.isNotEmpty ||
+                              club.localPhotoPath.isNotEmpty) {
+                            return BigClubCard(
+                              key: ValueKey(club.clubID),
+                              club: club,
+                              screenHeight: screenHeight,
+                              screenWidth: screenWidth,
+                            );
+                          }
+                          return null;
+                        },
+                      );
+                    },
+                  ),
+                  const Text(
+                    textAlign: TextAlign.center,
+                    'Περισσότερα club έρχονται σύντομα...',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: screenHeight / 40 * 6)
+                ],
               ),
             ),
           )),
--- Frontend/lib/MainPages/ProfilePage/ProfilePageOptions/my_bookings.dart
+++ Frontend/lib/MainPages/ProfilePage/ProfilePageOptions/my_bookings.dart
@@ -8,6 +8,8 @@ import 'package:provider/provider.dart';
 import '../../../Navigation/bottom_nav_bar.dart';
 import '../../../Providers/booking_provider.dart';
 import '../../../Providers/club_provider.dart';
+import '../../../Providers/global_state_provider.dart';
+import '../../../Providers/user_provider.dart';
 import '../../../global_components.dart';
 import '../../../routes/app_router.gr.dart';
 
@@ -31,6 +33,11 @@ class _MyBookingsPageState extends State<MyBookingsPage>
     // Delay fetchBookings until after the first frame is rendered
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<BottomNavBarVisibility>().hide();
+      if (context.read<GlobalStateProvider>().isAuthenticated) {
+        context.read<BookingProvider>().fetchBookings(
+            context.read<UserProvider>().userDetails,
+            context.read<ClubProvider>());
+      }
     });
   }
 
